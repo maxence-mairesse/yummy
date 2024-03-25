b26de28b420bafc48f5865f8c629639bea29d8ac
Moscato1 <mairessemax@live.fr>
2024-03-25 15:17:34 +0100

    css page details

--- app/Views/header.tpl.php
+++ app/Views/header.tpl.php
@@ -4,7 +4,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="public/asset/favicon/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/public/asset/favicon/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="/public/asset/CSS/reset.css">
     <link rel="stylesheet" href="/public/asset/CSS/style.css">
     <link rel="stylesheet" href="/public/asset/CSS/header.css">
